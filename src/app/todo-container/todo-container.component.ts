58e91349dd4cfcb4f21d7ca194ab2055d375df97
Steven Isbell <steveni_92@msn.com>
2019-04-29 23:04:00 -0600

    styles

--- src/app/todo-container/todo-container.component.ts
+++ src/app/todo-container/todo-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { todos } from './mockTodos';
+import { Todo } from '../types/Todo';
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
@@ -8,4 +9,11 @@ import { todos } from './mockTodos';
 export class TodoContainerComponent implements OnInit {
   todos = todos;
   ngOnInit() {}
+  handleComplete(id) {
+    const itemIndex: number = this.todos.findIndex(todo => todo.id === id);
+    if (itemIndex === -1) {
+      throw new Error('Error Removing Item');
+    }
+    this.todos[itemIndex].completed = !this.todos[itemIndex].completed;
+  }
 }
