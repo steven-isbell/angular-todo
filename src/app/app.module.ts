7e04e24756a6767e338c55d4827bdc8f2bf7ed03
Steven Isbell <steveni_92@msn.com>
2019-04-30 21:05:42 -0600

    add-todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoContainerComponent } from './todo-container/todo-container.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoContainerComponent],
+  declarations: [AppComponent, TodoContainerComponent, AddTodoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
